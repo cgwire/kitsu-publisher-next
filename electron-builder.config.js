07cf27ba6bb930dab8996a0ab61f179532284614
Frank Rousseau <frank.rousseau@free.fr>
2021-09-14 10:27:57 +0200

    [qa] Configure electron-builder

---
+++ electron-builder.config.js
@@ -0,0 +1,24 @@
+if (process.env.VITE_APP_VERSION === undefined) {
+  const now = new Date;
+  process.env.VITE_APP_VERSION = `${now.getUTCFullYear() - 2000}.${now.getUTCMonth() + 1}.${now.getUTCDate()}-${now.getUTCHours() * 60 + now.getUTCMinutes()}`;
+}
+
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const config = {
+  directories: {
+    output: 'build',
+    buildResources: 'buildResources',
+  },
+  files: [
+    './dist/**',
+    './electron/**',
+  ],
+  extraMetadata: {
+    version: process.env.VITE_APP_VERSION,
+  },
+};
+
+module.exports = config;
