9a7fcbddf271277a00be9bcb3265dbd3d51d32e5
Evan Blaudy <evan@cg-wire.com>
2021-09-22 22:38:46 +0200

    [qa] add eslint + prettier

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import "./src/styles/variables.scss";'
-      }
-    }
+        additionalData: '@import "./src/styles/variables.scss";',
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
-    extensions: ['.vue', '.js']
+    extensions: [".vue", ".js"],
   },
-  base: path.join(__dirname, './dist/'),
-})
-
+  base: path.join(__dirname, "./dist/"),
+});
