f5bc4a3d4b3b6a000e97cd6eb7bfe8e8055e97a5
Evan Blaudy <evan@cg-wire.com>
2021-09-23 21:31:33 +0200

    [qa] config for eslint + upgrade for packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    //"vue/no-unused-vars": "off",
+    //'vue/no-deprecated-slot-attribute': 'off',
   },
   globals: {
     defineProps: "readonly",
