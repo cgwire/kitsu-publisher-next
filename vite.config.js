3c9f108e997300e3fb2c9d214747699b1d7ac533
Evan Blaudy <evan@cg-wire.com>
2021-09-28 11:59:32 +0200

    [qa] remove vue-compat alias

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      vue: '@vue/compat'
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
   },
