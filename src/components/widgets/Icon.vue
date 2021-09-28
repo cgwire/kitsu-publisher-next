fa86ed0edcaa21bd087f970cb12ce856de80a02e
Evan Blaudy <evan@cg-wire.com>
2021-09-28 12:21:52 +0200

    [qa] remove vue-feather use a widget instead since it's not compatible with vue3

---
+++ src/components/widgets/Icon.vue
@@ -0,0 +1,39 @@
+<template>
+<span>
+  <vue-feather
+    :size="width"
+    :type="type"
+  />
+</span>
+</template>
+
+<script>
+export default {
+  name: 'icon',
+  components: {
+  },
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: Number,
+      default: 16
+    }
+  },
+
+  computed: {
+    type () {
+      return this.name.length > 0 ? this.name : 'plus'
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
