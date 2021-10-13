92562ba19dde9614b58b1cc76ef6651475b250a5
Frank Rousseau <frank.rousseau@free.fr>
2021-10-13 15:38:59 +0200

    [qa] Remove CORS

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,11 @@
-const { app, BrowserWindow } = require('electron')
+const { app, remote, BrowserWindow } = require('electron')
 const path = require('path')
+
+app.commandLine.appendSwitch("disable-features", "OutOfBlinkCors")
+
+const filter = {
+  urls: ['*://*.google.com/*']
+};
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -15,6 +21,34 @@ function createWindow() {
     win.loadURL('http://localhost:3000')
     win.webContents.openDevTools()
   } else win.loadFile(path.join(__dirname, '../dist/index.html'))
+
+  // Modify the origin for all requests to the following urls.
+  /*
+  const session = win.webContents.session
+  const filter = {
+    urls: ['http://example.com/*']
+  }
+
+  session.defaultSession.webRequest.onBeforeSendHeaders(
+    filter,
+    (details, callback) => {
+      console.log(details)
+      details.requestHeaders['Origin'] = 'http://example.com'
+      callback({ requestHeaders: details.requestHeaders })
+    }
+  )
+
+  session.defaultSession.webRequest.onHeadersReceived(
+    filter,
+    (details, callback) => {
+      console.log(details)
+      details.responseHeaders['Access-Control-Allow-Origin'] = [
+        'capacitor-electron://-'
+      ]
+      callback({ responseHeaders: details.responseHeaders })
+    }
+  )
+  */
 }
 
 app.whenReady().then(() => {
