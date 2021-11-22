54510da297ce5e371dda0f2a0ec0f3ef82d08f97
Evan Blaudy <evan@cg-wire.com>
2021-11-22 20:37:37 +0100

    remove unused comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,16 +18,5 @@ export default defineConfig({
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
   },
-  /*server: {
-    proxy: {
-      '/api': {
-        target: process.env.KITSU_API_TARGET || 'http://localhost:5000',
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/socket.io': {
-        target: process.env.KITSU_EVENT_TARGET || 'http://127.0.0.1:5001'
-      }
-    }
-  },*/
   base: './'
 })
