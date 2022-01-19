ccd4483ea9625bdd5ec576eda3fc8cc627da96ac
Evan BLAUDY <evan@cg-wire.com>
2022-01-19 05:53:06 +0100

    [qa] lint & format

--- app/renderer/src/components/mixins/entity.js
+++ app/renderer/src/components/mixins/entity.js
@@ -1,59 +1,54 @@
-import {
-  getFirstStartDate,
-  getLastEndDate,
-  parseDate
-} from '@/lib/time'
+import { getFirstStartDate, getLastEndDate, parseDate } from '@/lib/time'
 import moment from 'moment'
 
 /*
  * Common functions to shot and asset pages.
  */
 export const entityMixin = {
+  created() {},
 
-  created () {
-  },
+  mounted() {},
 
-  mounted () {
-  },
-
-  beforeDestroy () {
-  },
+  beforeDestroy() {},
 
   computed: {
-    currentTasks () {
+    currentTasks() {
       const entity = this.currentAsset || this.currentShot
       return entity
-        ? entity
-          .tasks
-          .map(taskId => this.taskMap.get(taskId))
-          .filter(task => task)
-          .sort((a, b) => {
-            const taskTypeAPriority = this.getTaskTypePriority(a.task_type_id)
-            const taskTypeBPriority = this.getTaskTypePriority(b.task_type_id)
-            return taskTypeAPriority - taskTypeBPriority
-          })
+        ? entity.tasks
+            .map((taskId) => this.taskMap.get(taskId))
+            .filter((task) => task)
+            .sort((a, b) => {
+              const taskTypeAPriority = this.getTaskTypePriority(a.task_type_id)
+              const taskTypeBPriority = this.getTaskTypePriority(b.task_type_id)
+              return taskTypeAPriority - taskTypeBPriority
+            })
         : []
     },
 
-    tasksStartDate () {
-      if (this.scheduleItems.length > 0 &&
-          this.scheduleItems[0].children.length > 0) {
+    tasksStartDate() {
+      if (
+        this.scheduleItems.length > 0 &&
+        this.scheduleItems[0].children.length > 0
+      ) {
         return getFirstStartDate(this.scheduleItems[0].children)
       } else {
         return moment()
       }
     },
 
-    tasksEndDate () {
-      if (this.scheduleItems.length > 0 &&
-          this.scheduleItems[0].children.length > 0) {
+    tasksEndDate() {
+      if (
+        this.scheduleItems.length > 0 &&
+        this.scheduleItems[0].children.length > 0
+      ) {
         return getLastEndDate(this.scheduleItems[0].children)
       } else {
         return moment().add(30, 'days')
       }
     },
 
-    scheduleItems () {
+    scheduleItems() {
       let manDays = 0
       const rootElement = {
         avatar: false,
@@ -68,49 +63,56 @@ export const entityMixin = {
         editable: false
       }
       const limitStartDate = moment()
-      const children = this.currentTasks.map(task => {
-        const estimation = this.formatDuration(task.estimation)
-        let startDate = limitStartDate.clone()
-        let endDate
+      const children = this.currentTasks
+        .map((task) => {
+          const estimation = this.formatDuration(task.estimation)
+          let startDate = limitStartDate.clone()
+          let endDate
 
-        if (!task.start_date && !task.real_start_date &&
-            !task.due_date && !task.end_date) return null
+          if (
+            !task.start_date &&
+            !task.real_start_date &&
+            !task.due_date &&
+            !task.end_date
+          )
+            return null
 
-        if (task.start_date) {
-          startDate = parseDate(task.start_date)
-        } else if (task.real_start_date) {
-          startDate = parseDate(task.real_start_date)
-        }
+          if (task.start_date) {
+            startDate = parseDate(task.start_date)
+          } else if (task.real_start_date) {
+            startDate = parseDate(task.real_start_date)
+          }
 
-        if (task.due_date) {
-          endDate = parseDate(task.due_date)
-        } else if (task.end_date) {
-          endDate = parseDate(task.end_date)
-        } else if (task.estimation) {
-          endDate = startDate.clone().add(estimation, 'days')
-        }
+          if (task.due_date) {
+            endDate = parseDate(task.due_date)
+          } else if (task.end_date) {
+            endDate = parseDate(task.end_date)
+          } else if (task.estimation) {
+            endDate = startDate.clone().add(estimation, 'days')
+          }
 
-        if (!endDate || endDate.isBefore(startDate)) {
-          endDate = startDate.clone().add(1, 'days')
-        }
-        if (estimation) manDays += task.estimation
-        const taskType = this.taskTypeMap.get(task.task_type_id)
+          if (!endDate || endDate.isBefore(startDate)) {
+            endDate = startDate.clone().add(1, 'days')
+          }
+          if (estimation) manDays += task.estimation
+          const taskType = this.taskTypeMap.get(task.task_type_id)
 
-        return {
-          ...task,
-          name: taskType.name,
-          startDate: startDate,
-          endDate: endDate,
-          expanded: false,
-          loading: false,
-          man_days: estimation,
-          editable: false,
-          unresizable: false,
-          parentElement: rootElement,
-          color: taskType.color,
-          children: []
-        }
-      }).filter(c => c !== null)
+          return {
+            ...task,
+            name: taskType.name,
+            startDate: startDate,
+            endDate: endDate,
+            expanded: false,
+            loading: false,
+            man_days: estimation,
+            editable: false,
+            unresizable: false,
+            parentElement: rootElement,
+            color: taskType.color,
+            children: []
+          }
+        })
+        .filter((c) => c !== null)
       let rootStartDate = moment()
       let rootEndDate = moment().add('days', 1)
       if (children.length > 0) {
@@ -127,6 +129,5 @@ export const entityMixin = {
     }
   },
 
-  methods: {
-  }
+  methods: {}
 }
