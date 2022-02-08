3e7d43565da3a1abdc46861a1230d656a1290f17
Evan Blaudy <evan@cg-wire.com>
2022-02-08 02:24:45 +0100

    [fix][connectors][blender] fix autoenabling addon in Blender

--- connectors/blender/auto-enable.py
+++ connectors/blender/auto-enable.py
@@ -1,5 +1,9 @@
 import addon_utils
 import bpy
 
-addon_utils.enable("kitsu-publisher", default_set=True)
+list_addons_enabled = [addon.module for addon in bpy.context.preferences.addons]
+if not "kitsu-publisher" in list_addons_enabled:
+    addon_utils.enable("kitsu-publisher", default_set=True)
+    bpy.ops.wm.save_userpref()
+
 bpy.ops.wm.quit_blender()
