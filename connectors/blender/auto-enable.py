0cf4bf1f664c6024a6c5f26c273969e5f4985f4c
EvanBldy <evan@cg-wire.com>
2022-10-04 00:54:23 +0200

    [qa] move dccutils_server in an pypi package

--- connectors/blender/auto-enable.py
+++ connectors/blender/auto-enable.py
@@ -1,7 +1,9 @@
 import addon_utils
 import bpy
 
-list_addons_enabled = [addon.module for addon in bpy.context.preferences.addons]
+list_addons_enabled = [
+    addon.module for addon in bpy.context.preferences.addons
+]
 if not "kitsu-publisher" in list_addons_enabled:
     addon_utils.enable("kitsu-publisher", default_set=True)
     bpy.ops.wm.save_userpref()
--- connectors/blender/dccutils_server/__init__.py
+++
@@ -1,2 +0,0 @@
-from .__version__ import __version__
-from .server import server_start, server_start_threading
